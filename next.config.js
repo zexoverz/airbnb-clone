a36b93f35c2608fff03e13eff504c5ad950c18c8
faisal <faisalfirdani01@gmail.com>
2023-06-03 12:38:42 +0700

    Finishing Migration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: [
+      'res.cloudinary.com', 
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com'
+    ]
+  }
 }
 
 module.exports = nextConfig
